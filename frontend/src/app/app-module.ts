8cc5c513bcb71581f22b3255383a4dac2d980a94
nour mamdouh <nourmamdouh54@gmail.com>
2025-10-15 17:01:20 +0300

    api service and app modules and api models

--- frontend/src/app/app-module.ts
+++ frontend/src/app/app-module.ts
@@ -1,5 +1,7 @@
 import { NgModule, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { BrowserModule, provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
@@ -14,7 +16,9 @@ import { Login } from './login/login';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,   // ضروري للـ API calls
+    FormsModule         // لو عندك [(ngModel)] في login وغيره
   ],
   providers: [
     provideBrowserGlobalErrorListeners(),
