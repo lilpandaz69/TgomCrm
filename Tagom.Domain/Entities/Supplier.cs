c82b09c060509833c404f0e911415dd98be0fa59
MariooX20 <attack6599@gmail.com>
2025-10-18 19:19:14 +0300

    Phone Validation#2

--- Tagom.Domain/Entities/Supplier.cs
+++ Tagom.Domain/Entities/Supplier.cs
@@ -1,9 +1,13 @@
-﻿namespace Tagom.Domain.Entities
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Tagom.Domain.Entities
 {
     public class Supplier 
     {
         public int SupplierId { get; set; }
         public string Name { get; set; } = null!;
+        [Phone,MinLength(11),MaxLength(11)]
+        [RegularExpression(@"^01[0-2,5]{1}[0-9]{8}$", ErrorMessage = "Invalid phone number")]
         public string? Phone { get; set; }
     }
 
